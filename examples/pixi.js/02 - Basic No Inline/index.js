e767695b7f7e59f9d6f2ffae9264207ccd51e163
anvaka <anvaka@gmail.com>
2014-01-04 15:47:13 -0800

    Showing refactored example

---
+++ examples/pixi.js/02 - Basic No Inline/index.js
@@ -0,0 +1,28 @@
+module.exports.main = function () {
+  var graph = require('ngraph.generators').grid(40, 40),
+      layout = createLayout(graph);
+
+  var createPixiGraphics = require('./pixiGraphics');
+  var graphics = createPixiGraphics(graph, layout);
+
+  // begin animation loop:
+  renderFrame();
+
+  function renderFrame() {
+    graphics.renderFrame();
+    requestAnimFrame(renderFrame);
+  }
+}
+
+function createLayout(graph) {
+  var layout = require('ngraph.forcelayout'),
+      physics = require('ngraph.physics.simulator');
+
+  return layout(graph, physics({
+          springLength: 30,
+          springCoeff: 0.0008,
+          dragCoeff: 0.01,
+          gravity: -1.2,
+          theta: 1
+        }));
+}

05ba2ad483409be5b5dca12624c9819306b6c51e
anvaka <anvaka@gmail.com>
2014-01-16 19:50:14 -0800

    Added example with fabric.js

---
+++ examples/fabric.js/Node and Browser/index.js
@@ -0,0 +1,23 @@
+module.exports.main = function () {
+  var queryString = typeof window !== 'undefined' ? window.location.search.substring(1) : '';
+  var graph = getGraphFromQueryString(queryString); 
+  var fabricGraphics = require('ngraph.fabric')(graph);
+
+  // this is a power of fabric. Shared ui settings can be used both on the
+  // server side inside node.js application and in the browser. Checkout
+  // `node.js` example to see how it's used inside node
+  require('./ui')(fabricGraphics, fabric);
+
+  // begin animation loop:
+  fabricGraphics.run();
+};
+
+function getGraphFromQueryString(queryString) {
+  var query = require('query-string').parse(queryString);
+  var n = parseInt(query.n, 10) || 10;
+  var m = parseInt(query.m, 10) || 10;
+
+  var graphGenerators = require('ngraph.generators')
+  var createGraph = graphGenerators[query.graph] || graphGenerators.grid;
+  return createGraph(n, m);
+}
---
+++ examples/fabric.js/Node and Browser/ui.js
@@ -0,0 +1,55 @@
+// this code changes appearance of nodes and links for fabricGraphics. What's
+// nice about it - it is used in `customUI` example which renders interactive
+// graphs in browser, and in `node.js` example to render graph into the image
+module.exports = function (fabricGraphics, fabric) {
+  fabricGraphics.createNodeUI(createNode)
+    .renderNode(renderNode)
+    .createLinkUI(createLink)
+    .renderLink(renderLink);
+
+  return;
+
+  function createNode(node) {
+    return new fabric.Circle({ radius: Math.random() * 20, fill: getNiceColor() });
+  }
+
+  function renderNode(circle) {
+    circle.left = circle.pos.x - circle.radius;
+    circle.top = circle.pos.y - circle.radius;
+  }
+
+  function createLink(link) {
+    // lines in fabric are odd... Maybe I don't understand them.
+    return new fabric.Line([0, 0, 0, 0], {
+      stroke: getNiceColor(),
+      originX: 'center',
+      originY: 'center'
+    });
+  }
+
+  function renderLink(line) {
+    line.set({
+      x1: line.from.x,
+      y1: line.from.y,
+      x2: line.to.x,
+      y2: line.to.y
+    });
+  }
+}
+
+var niceColors = [
+ '#1f77b4', '#aec7e8',
+ '#ff7f0e', '#ffbb78',
+ '#2ca02c', '#98df8a',
+ '#d62728', '#ff9896',
+ '#9467bd', '#c5b0d5',
+ '#8c564b', '#c49c94',
+ '#e377c2', '#f7b6d2',
+ '#7f7f7f', '#c7c7c7',
+ '#bcbd22', '#dbdb8d',
+ '#17becf', '#9edae5'
+];
+
+function getNiceColor() {
+  return niceColors[(Math.random() * niceColors.length)|0];
+}

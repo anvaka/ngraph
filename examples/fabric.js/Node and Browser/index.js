2f174f7debd43ff00281dda45a1d08380aa97cb6
anvaka <anvaka@gmail.com>
2014-01-16 22:14:22 -0800

    Made it even simpler

--- examples/fabric.js/Node and Browser/index.js
+++ examples/fabric.js/Node and Browser/index.js
@@ -1,6 +1,5 @@
 module.exports.main = function () {
-  var queryString = typeof window !== 'undefined' ? window.location.search.substring(1) : '';
-  var graph = getGraphFromQueryString(queryString); 
+  var graph = getGraphFromQueryString(window.location.search.substring(1));
   var fabricGraphics = require('ngraph.fabric')(graph);
 
   // this is a power of fabric. Shared ui settings can be used both on the
@@ -17,7 +16,7 @@ function getGraphFromQueryString(queryString) {
   var n = parseInt(query.n, 10) || 10;
   var m = parseInt(query.m, 10) || 10;
 
-  var graphGenerators = require('ngraph.generators')
+  var graphGenerators = require('ngraph.generators');
   var createGraph = graphGenerators[query.graph] || graphGenerators.grid;
   return createGraph(n, m);
 }

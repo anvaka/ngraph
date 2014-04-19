035c17f571a9544a36eada2bc78bd6ec6e1a5123
anvaka <anvaka@gmail.com>
2014-04-19 16:15:19 -0700

    Updated colors

--- examples/three.js/UFL/index.js
+++ examples/three.js/UFL/index.js
@@ -1,4 +1,4 @@
-var query, z;
+var query, z, layout;
 
 module.exports.main = function () {
   query = require('query-string').parse(window.location.search.substring(1));
@@ -15,9 +15,11 @@ function renderGraph(graph) {
   renderer.onFrame(animateScene(scene, camera, lights));
   renderer.createNodeUI(nodeUI).createLinkUI(linkUI);
 
+  layout = renderer.layout;
   renderer.run(); // begin animation loop:
   var maxDegreeNode = getMaxDegree(graph);
   renderer.layout.pinNode(maxDegreeNode, true);
+
 }
 
 function getMaxDegree(graph) {
@@ -48,19 +50,22 @@ function getNumber(string, defaultValue) {
 
 
 function nodeUI(node) {
-  var nodeGeometry = new THREE.SphereGeometry(4);
+  var nodeGeometry = new THREE.SphereGeometry(2);
   var nodeMaterial = new THREE.MeshPhongMaterial({
-    color: 0x00BFFF
+    color: 0xFF0100
   });
   return new THREE.Mesh(nodeGeometry, nodeMaterial);
 }
 
+var links = [];
 function linkUI(link) {
   var linkGeometry = new THREE.Geometry();
   linkGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
   linkGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
 
   var linkMaterial = new THREE.LineBasicMaterial({ color: 0x333333 });
+  link.color = linkMaterial.color;
+  links.push(link);
   return new THREE.Line(linkGeometry, linkMaterial);
 }
 
@@ -73,6 +78,7 @@ function addLights(scene) {
 
 function animateScene(scene, camera, lights) { 
   return function () {
+    updateColors();
     var timer = Date.now() * 0.0002;
     camera.position.x = Math.cos(timer) * z;
     camera.position.z = Math.sin(timer) * z;
@@ -80,4 +86,61 @@ function animateScene(scene, camera, lights) {
     lights.position.z = Math.sin(timer);
     camera.lookAt(scene.position);
   };
+}
+
+function updateColors() {
+  if (!links.length) return;
+
+  // first calculate avg link length:
+  var minLength = Number.POSITIVE_INFINITY, maxLength = 0;
+  var link;
+  var avgDistance = 0;
+  for (var i = 0; i < links.length; ++i) {
+    link = links[i];
+
+    var pos = layout.getLinkPosition(link.id);
+    var from = pos.from;
+    var to = pos.to;
+    link.distance = Math.sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y) + (from.z - to.z) * (from.z - to.z));
+    avgDistance += link.distance;
+
+    if (link.distance < minLength) minLength = link.distance;
+    if (link.distance > maxLength) maxLength = link.distance;
+  }
+
+  avgDistance /= links.length;
+
+  for (i = 0; i < links.length; ++i) {
+    link = links[i];
+    var dt = link.distance - avgDistance;
+    var hue = 90 * (dt * 4/(avgDistance) + 1);
+    hue = Math.min(Math.max(hue, 0), 180);
+    var c = hsbToRgb(hue, 100, 100);
+    link.color.r = c.r;
+    link.color.g = c.g;
+    link.color.b = c.b;
+  }
+}
+
+function hsbToRgb(h, s, b){
+  var r, g;
+  var br = Math.round(b / 100 * 255);
+  if (s === 0){
+    r = br; g =  br; b = br;
+  } else {
+    var hue = h % 360;
+    var f = hue % 60;
+    var p = Math.round((b * (100 - s)) / 10000 * 255);
+    var q = Math.round((b * (6000 - s * f)) / 600000 * 255);
+    var t = Math.round((b * (6000 - s * (60 - f))) / 600000 * 255);
+    switch(Math.floor(hue / 60)){
+      case 0: r = br; g = t; b = p; break;
+      case 1: r = q; g = br; b = p; break;
+      case 2: r = p; g = br; b = t; break;
+      case 3: r = p; g =  q; b = br; break;
+      case 4: r = t; g = p; b = br; break;
+      case 5: r = br; g =  p; b = q; break;
+    }
+  }
+  return {r : r/255, g: g/255, b: b/255};
 }
